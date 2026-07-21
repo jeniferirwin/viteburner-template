e5f5b6a7500f726b094b5ef89502e6885d5f11c7
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-07-20 21:16:25 -0400

    refactor: replace hardcoded values with a constant for hacknet upgrades

--- src/scripts/hacknet.ts
+++ src/scripts/hacknet.ts
@@ -1,21 +1,22 @@
 import {NS} from "@ns";
 
 export async function main(ns: NS) {
+    const pct = 0.05;
     while (true) {
-        if (ns.hacknet.getPurchaseNodeCost() < 0.2 * ns.getPlayer().money) {
+        if (ns.hacknet.getPurchaseNodeCost() < pct * ns.getPlayer().money) {
             ns.hacknet.purchaseNode();
         }
         for (var i = 0; i < ns.hacknet.numNodes(); i++) {
-            if (ns.hacknet.getLevelUpgradeCost(i) < 0.2 * ns.getPlayer().money) {
+            if (ns.hacknet.getLevelUpgradeCost(i) < pct * ns.getPlayer().money) {
                 ns.hacknet.upgradeLevel(i);
             }
-            if (ns.hacknet.getRamUpgradeCost(i) < 0.2 * ns.getPlayer().money) {
+            if (ns.hacknet.getRamUpgradeCost(i) < pct * ns.getPlayer().money) {
                 ns.hacknet.upgradeRam(i);
             }
-            if (ns.hacknet.getCoreUpgradeCost(i) < 0.2 * ns.getPlayer().money) {
+            if (ns.hacknet.getCoreUpgradeCost(i) < pct * ns.getPlayer().money) {
                 ns.hacknet.upgradeCore(i);
             }
         }
-        await ns.sleep(100);
+        await ns.sleep(1000);
     }
 }
