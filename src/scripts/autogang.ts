b70c0d7e95cc1ad0a15f1ec43a27b21b59457d57
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-07-25 16:11:49 -0400

    feat: refactor autogang script to include BuyMemberGear function and adjust task assignment flow

--- src/scripts/autogang.ts
+++ src/scripts/autogang.ts
@@ -1,5 +1,18 @@
 import {NS} from "@ns";
 
+export function BuyMemberGear(ns: NS, members: Array<string>) {
+	var gear = ns.gang.getEquipmentNames();
+	for (var memberName of members) {
+		var member = ns.gang.getMemberInformation(memberName);
+		for (var piece of gear) {
+			ns.tprintRaw(`Checking cost of ${piece}`);
+			if (ns.gang.getEquipmentCost(piece) < ns.getPlayer().money * 0.05) {
+				ns.gang.purchaseEquipment(memberName, piece);
+			}
+		}
+	}
+}
+
 export function AscendReadyMembers(ns: NS, members: Array<string>) {
     for (var memberName of members) {
         var ascension = ns.gang.getAscensionResult(memberName);
@@ -23,7 +36,7 @@ export function AssignMemberTasks(ns: NS, members: Array<string>) {
             ["hack", member.hack_asc_mult],
             ["str", member.str_asc_mult],
             ["dex", member.dex_asc_mult],
-            ["def", member.def_asc_mult],
+            ["def", member.def_asc_mult]
         ];
         var lowest = mults
             .reduce((min, entry) => (entry[1] > 0 && entry[1] < min[1]) ? entry : min)[0];
@@ -42,6 +55,9 @@ export function AssignMemberTasks(ns: NS, members: Array<string>) {
 export async function main(ns: NS) {
     while (true) {
         var names = ns.gang.getMemberNames();
+		AscendReadyMembers(ns, names);
+		AssignMemberTasks(ns, names);
+		BuyMemberGear(ns, names);	
         await ns.gang.nextUpdate();
     }
 }
