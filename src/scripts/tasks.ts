7454da8b958a3c0f9479d0e3bdfde3a3b2a4163d
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-07-27 02:45:31 -0400

    refactor: simplify TaskAssignment and AttackAssignment constructors, enhance victim validation and agent assignment logic

--- src/scripts/tasks.ts
+++ src/scripts/tasks.ts
@@ -1,79 +1,120 @@
 import {ScriptArg, NS} from "@ns";
 import { Globals } from "./globals";
-
-export enum TaskType {
-    WEAKEN,
-    GROW,
-    HACK
-}
+import { getServerXT, ServerXT } from "./serverxt";
+import { get, Server } from "http";
+import { getAllServers } from "./libserver";
 
 export class TaskAssignment {
-    pid: number = -1;
+    pid?: number;
 
     protected constructor(
-        public type: TaskType,
         public args?: ScriptArg[]
-    ) {}
+    ) {};
 
     taskLaunched() {
-        return this.pid > -1;
+        return (this.pid ?? -1) > -1;
     }
 
-    static create(type: TaskType, args?: ScriptArg[]): TaskAssignment | undefined {
-        return new TaskAssignment(type, args);
+    static create(args?: ScriptArg[]): TaskAssignment | undefined {
+        return new TaskAssignment(args);
     }
 }
-export class AttackAssignment extends TaskAssignment {
-    private agent: string = "";
 
+export class AttackAssignment extends TaskAssignment {
+    public agent?: string;
+    public threads?: number;
+    public time?: number;
     protected constructor(
         public ns: NS,
-        public type: TaskType,
-        public victim: string
+        public victimName: string
     ) {
-        super(type);
+        super();
     }
 
-    static createAttack(ns: NS, type: TaskType, victim: string): AttackAssignment | undefined {
+    static createAttack(ns: NS, victim: string): AttackAssignment | undefined {
         if (!AttackAssignment.validateVictim(ns, victim)) return undefined;
-        return new AttackAssignment(ns, type, victim);
+        return new AttackAssignment(ns, victim);
     }
 
-    start(ns: NS) {
-        if (this.taskLaunched()) return;
-        // TODO
+    getVictimXT(ns: NS): ServerXT | undefined {
+        return getServerXT(ns, this.victimName);
     }
 
-    private getTaskScript(ns: NS): string | undefined {
-        switch (this.type) {
-            case TaskType.WEAKEN:
-                return Globals.scriptWeaken;
-            case TaskType.GROW:
-                return Globals.scriptGrow;
-            case TaskType.HACK:
-                return Globals.scriptHack;
-            default:
-                return undefined;
-        }
-    }
-
-    private static validateVictim(ns: NS, victim: string): boolean {
-        if (ns.serverExists(victim) && ns.hasRootAccess(victim)) return true;
+    static validateVictim(ns: NS, victim: string): boolean {
+        if (!ns.serverExists(victim) || ns.getServerMaxMoney(victim) === 0 || ns.getServerMinSecurityLevel(victim) < 0) return false;
+        var skill = ns.getPlayer().skills.hacking;
+        var required = ns.getServerRequiredHackingLevel(victim);
+        if (skill >= required) return true;
         return false;
     }
 
-    public assignAgent(ns: NS, hostname: string): boolean {
-        if (!ns.serverExists(hostname)) return false;
-        var script = this.getTaskScript(ns) ?? "";
-        if (!ns.fileExists(script, hostname)) {
-            return false;
-        }
-        this.agent = hostname;
+    public setAgent(ns: NS, agentName: string): boolean {
+        if (!ns.serverExists(agentName)) return false;
+        var server = getServerXT(ns, agentName);
+        if (!server?.isAgent()) return false;
+        this.agent = agentName;
         return true;
     }
+}
 
-    private static validateThreads(threads: number): boolean {
-        if (threads > 0) return true;
-        return false;
+export class ZeroSecDiff extends AttackAssignment {
+    public secDiff?: number;
+
+    constructor(
+        public ns: NS,
+        public victimName: string,
+    ) {
+        super(ns, victimName);
     }
+
+    static createAttack(ns: NS, victim: string): ZeroSecDiff | undefined {
+        if (!ZeroSecDiff.validateVictim(ns, victim)) return undefined;
+        return new ZeroSecDiff(ns, victim);
+    }
+
+    public calculateThreads(ns: NS, attacker: ServerXT, target: ServerXT): number {
+        var diff = target.getSecurityDiff(ns) ?? 0;
+        this.threads = Math.ceil(diff / 0.05);
+        var amount = ns.weakenAnalyze(this.threads ?? 0, attacker.cpuCores ?? 1);
+        while (ns.weakenAnalyze((this.threads ?? 0), attacker.cpuCores ?? 1) > diff) {
+            this.threads--;
+        }
+        this.threads++;
+        var final = ns.weakenAnalyze((this.threads ?? 0), attacker.cpuCores);
+        ns.tprint(`${attacker.hostname} needs ${this.threads} threads to do ${final} reduction vs ${diff} (${attacker.cpuCores} cores)`)
+        return this.threads;
+    }
+
+    public generateAttackPlan(ns: NS, agentName: string): boolean {
+        if (!this.setAgent(ns, agentName) || this.agent === undefined) return false;
+        var attacker = getServerXT(ns, this.agent);
+        var target = getServerXT(ns, this.victimName);
+        return true;
+    }
+}
+
+export function main(ns: NS) {
+    var test = ZeroSecDiff.createAttack(ns, ns.args[0] as string);
+    var target = test?.getVictimXT(ns);
+    if (test === undefined || target === undefined) return;
+    var servers = getAllServers(ns);
+    let threads;
+    let serverName;
+    var scriptRam = ns.getScriptRam("scripts/atk_weaken.js", "home");
+    let ram;
+    for (var [hostname, server] of servers) {
+        if (!server.isAgent() || server === undefined) continue;
+        if (threads === undefined) {
+            threads = test.calculateThreads(ns, server, target);
+        }
+        var newcalc = test.calculateThreads(ns, server, target);
+        ns.tprintRaw(`${newcalc} < ${threads} && ${scriptRam} * ${threads} (${scriptRam * threads}) < ${server.openRAM() ?? 0}GB`);
+        if (newcalc <= threads && scriptRam * threads < (server.openRAM() ?? 0)) {
+            threads = newcalc;
+            serverName = hostname;
+            ns.tprintRaw(`setting servername to ${hostname}`);
+            ram = threads * scriptRam;
+        }
+    }
+    ns.tprintRaw(`Winner is ${serverName} with ${threads} threads and ${ram} ram requirement`);
 }
