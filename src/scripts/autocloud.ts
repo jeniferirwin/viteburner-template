790f9fbbc427b7af81f7b23b3f498856e6d3fcb2
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-07-27 02:45:18 -0400

    feat: remove unused CloudNames enum and optimize server purchase logic in autocloud.ts

--- src/scripts/autocloud.ts
+++ src/scripts/autocloud.ts
@@ -1,34 +1,4 @@
 import {NS} from "@ns";
-import { Globals } from "./globals.ts";
-
-enum CloudNames {
-    "alfa",
-    "bravo",
-    "charlie",
-    "delta",
-    "echo",
-    "foxtrot",
-    "golf",
-    "hotel",
-    "india",
-    "juliett",
-    "kilo",
-    "lima",
-    "mike",
-    "november",
-    "oscar",
-    "papa",
-    "quebec",
-    "romeo",
-    "sierra",
-    "tango",
-    "uniform",
-    "victor",
-    "whiskey",
-    "xray",
-    "yankee",
-    "zulu"
-};
 
 export async function main(ns: NS) {
     var servers = ns.cloud.getServerNames();
@@ -36,21 +6,19 @@ export async function main(ns: NS) {
         var tiers = getCloudTiers();
         if (servers.length < ns.cloud.getServerLimit()) {
             if (ns.cloud.getServerCost(2) < ns.getPlayer().money * 0.05) {
-                var name = CloudNames[servers.length + 1];
-                ns.cloud.purchaseServer(name, 2);
+                ns.cloud.purchaseServer("entropy", 2);
             }
         }
         for (var server of servers) {
             var nextTier = tiers.indexOf(ns.getServerMaxRam(server)) + 1;
             if (nextTier < tiers.length) {
                 var cost = ns.cloud.getServerUpgradeCost(server, tiers[nextTier]);
-                var ram = tiers[nextTier];
                 if (cost <= ns.getPlayer().money / servers.length) {
                     ns.cloud.upgradeServer(server, tiers[nextTier]);
                 }   
             }
         }
-        await ns.sleep(10000);
+        await ns.sleep(100);
     }
 }
 
