88efd6cefef5db33548bf468e869b29f277a283a
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-07-28 20:30:53 -0400

    refactor: streamline ServerXT class by removing unused methods and simplifying constructor

--- src/scripts/serverxt.ts
+++ src/scripts/serverxt.ts
@@ -1,30 +1,26 @@
 import {NS, Server} from "@ns";
-import { Globals } from "./globals";
-import { TaskAssignment } from "./tasks";
 
 export interface ServerXT extends Server {
-    hasCash(): boolean;
-    openRAM(): number;
-    tryCrack(ns: NS): boolean;
-    putBundle(ns: NS): boolean;
+    crack(ns: NS): boolean;
+    getBundle(ns: NS): boolean;
+    getParent(ns: NS): string | undefined;
+    getChildren(ns: NS): Array<string> | undefined;
 }
 
 export class ServerXT implements Server, ServerXT {
-    hasCash(): boolean {
-        if (this.moneyMax ?? 0 > 0) return true;
-        return false;
+    private constructor(ns: NS, hostname: string) {
+        Object.assign(this, ns.getServer(hostname));
+        if (this.crack(ns)) this.getBundle(ns);
     }
 
-    openRAM(): number {
-        if ((this.maxRam ?? 0) > 0 && (this.ramUsed ?? -1) > -1) {
-            return (this.maxRam - this.ramUsed); 
-        }
-        return 0;
+    public static create(ns: NS, hostname: string): ServerXT | undefined {
+        if (!ns.serverExists(hostname)) return undefined;
+        return new ServerXT(ns, hostname);
     }
 
     crack(ns: NS): boolean {
+        if (this.hasAdminRights) return true;
         if (this.openPortCount === undefined || this.numOpenPortsRequired === undefined) return false;
-        if (this.openPortCount === 5) return true;
         ns.ftpcrack(this.hostname);
         ns.brutessh(this.hostname);
         ns.httpworm(this.hostname);
@@ -33,69 +29,17 @@ export class ServerXT implements Server, ServerXT {
         return ns.nuke(this.hostname);
     }
     
-    putBundle(ns: NS): boolean {
-        const bundle = ns.ls("home", "scripts");
-        return ns.scp(bundle, this.hostname, "home");
+    getBundle(ns: NS): boolean {
+        return ns.scp(ns.ls("home", "scripts"), this.hostname, "home");
     }
 
-    getParent(ns: NS): string | undefined {
-		var results = ns.scan(this.hostname);
-		return results[0];
-    }
-	
 	getChildren(ns: NS): Array<string> | undefined {
 		var results = ns.scan(this.hostname);
 		return results.slice(1);
 	}
 
-    getMoneyMult(ns: NS): number {
-        if (this.purchasedByPlayer ||
-            (this.moneyMax ?? 0) === 0 ||
-            (this.moneyAvailable ?? 0) === 0 ||
-            (this.baseDifficulty ?? -1) === -1 ||
-            (this.hackDifficulty ?? -1) === -1 ||
-            (this.minDifficulty ?? -1)) return 0;
-        return ns.getServerMaxMoney(this.hostname) / ns.getServerMoneyAvailable(this.hostname);
+    getParent(ns: NS): string | undefined {
+		var results = ns.scan(this.hostname);
+		return results[0];
     }
-
-    getSecurityDiff(ns: NS): number {
-        if (this.purchasedByPlayer) return -1;
-        if (this.hackDifficulty === undefined || this.minDifficulty === undefined) return -1;
-        return this.hackDifficulty - this.minDifficulty;
-    }
-
-    isAgent(): boolean {
-        if (this.hasAdminRights && (this.maxRam ?? 0) > 0) return true;
-        return false;
-    }
-
-    getWeakenRAM(ns: NS): number {
-        return ns.getScriptRam(Globals.scriptWeaken, this.hostname);
-    }
-
-    getGrowRAM(ns: NS): number {
-        return ns.getScriptRam(Globals.scriptGrow, this.hostname);
-    }
-
-    getHackRAM(ns: NS): number {
-        return ns.getScriptRam(Globals.scriptHack, this.hostname);
-    }
-
-    refresh(ns: NS): ServerXT {
-        var server = getServerXT(ns, this.hostname) ?? this;
-        return server;
-    }
-}
-
-/**
- * Get a ServerXT (Server Extended) with additional methods ready to go.
- * Automatically attempts to crack the server, and puts the script bundle
- * on it if successful.
- */
-export function getServerXT(ns: NS, hostname: string): ServerXT | undefined {
-    if (!ns.serverExists(hostname)) return undefined;
-    var raw = ns.getServer(hostname);
-    var xt = Object.assign(new ServerXT(), raw);
-    if (xt.crack(ns)) xt.putBundle(ns);
-    return xt;
 }
