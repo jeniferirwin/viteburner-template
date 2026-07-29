f64dfcaf4775936326a99e659d268e8d368f8afc
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-07-29 08:35:20 -0400

    feat: add Victim class with methods for security difference and money multiplier

---
+++ src/scripts/victim.ts
@@ -0,0 +1,38 @@
+import {NS} from "@ns";
+import { ServerXT, getServerXT } from "./serverxt";
+
+export interface Victim extends ServerXT {
+    getSecurityDiff(): number;
+    getMoneyMult(): number;
+}
+
+export class Victim implements Victim, ServerXT {
+    private constructor(ns: NS, hostname: string) {
+        Object.assign(this, getServerXT(ns, hostname));
+    }
+
+    public static create(ns: NS, hostname: string): Victim | undefined {
+        if (!Victim.isVictim(ns, hostname)) return undefined;
+        return new Victim(ns, hostname);
+    }
+
+    public static isVictim(ns: NS, hostname: string): boolean {
+        if (!ns.serverExists(hostname)) return false;
+        var server = ns.getServer(hostname);
+        if (server.purchasedByPlayer) return false;
+        if (!server.hasAdminRights) return false;
+        if (server.minDifficulty === undefined || server.hackDifficulty === undefined) return false;
+        if (server.openPortCount === undefined || server.numOpenPortsRequired === undefined) return false;
+        if (server.requiredHackingSkill ?? Number.POSITIVE_INFINITY > ns.getPlayer().skills.hacking) return false;
+        if (server.moneyMax === undefined || server.moneyAvailable === undefined) return false;
+        return true;
+    }
+
+    public getSecurityDiff(): number {
+        return this.hackDifficulty! - this.minDifficulty!;
+    }
+
+    public getMoneyMult(): number {
+        return this.moneyMax! / this.moneyAvailable!;
+    }
+}
