879df0a6a5ac5efdb74a76b10429d71317f75add
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-08-05 19:03:53 -0400

    feat: update caching mechanism to store server CPU cores instead of server names

--- src/scripts/daemon/cacher.ts
+++ src/scripts/daemon/cacher.ts
@@ -1,13 +1,22 @@
 import {NS} from "@ns";
 import { GetAllServerNames } from "../lib/server";
+import { CrackPorts, IsNukable, PutBundle } from "../lib/attack";
 
 export const CACHE_PORT = 1;
 
 export async function main(ns: NS) {
     while (true) {
         const servers = GetAllServerNames(ns);
+        const cache = new Map<string, number>();
+        for (const server of servers) {
+            if (!ns.hasRootAccess(server)) {
+                CrackPorts(ns, server);
+                if (IsNukable(ns, server)) if (ns.nuke(server)) PutBundle(ns, server);
+            }
+            cache.set(server, ns.getServer(server).cpuCores);
+        }
         ns.clearPort(CACHE_PORT);
-        ns.tryWritePort(CACHE_PORT, servers);
+        ns.tryWritePort(CACHE_PORT, cache);
         await ns.sleep(10000);
     }
 }
