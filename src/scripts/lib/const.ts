da2a33440465e4a65b065c22fd3f8de8c0e7cb73
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-08-06 19:56:55 -0400

    feat: add GetTotalRAM function for improved RAM calculation in scripts

--- src/scripts/lib/const.ts
+++ src/scripts/lib/const.ts
@@ -1,3 +1,5 @@
+import { NS } from "@ns";
+
 export const SCRIPTS = {
     hack: "/scripts/task/atk_hack.js",
     weaken: "/scripts/task/atk_weaken.js",
@@ -5,3 +7,9 @@ export const SCRIPTS = {
 }
 
 export const CACHE_PORT = 1;
+
+export function GetTotalRAM(ns: NS, script: string, threads: number = 1): number {
+    threads = Math.round(threads);
+    if (threads < 1 && threads >= Number.POSITIVE_INFINITY) return 0;
+    return threads * ns.getScriptRam(script, "home");
+}
