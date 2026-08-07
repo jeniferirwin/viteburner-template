1da9bd120b4bbe3d9fb235bd190e6b1f7ad165e6
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-08-06 20:26:07 -0400

    refactor: remove unused imports and constants from const.ts

--- src/scripts/lib/const.ts
+++ src/scripts/lib/const.ts
@@ -1,9 +1,5 @@
-import { NS } from "@ns";
-
 export const SCRIPTS = {
     hack: "/scripts/task/atk_hack.js",
     weaken: "/scripts/task/atk_weaken.js",
     grow: "/scripts/task/atk_grow.js"
 }
-
-export const CACHE_PORT = 1;
