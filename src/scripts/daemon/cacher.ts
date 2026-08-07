6b8d4663c2e7db5d50ea0bc596b61f02059bde61
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-08-06 20:20:30 -0400

    feat: enhance CacheEntry and CacheDB classes with improved server management and RAM calculations

--- src/scripts/daemon/cacher.ts
+++ src/scripts/daemon/cacher.ts
@@ -1,7 +1,13 @@
 import {NS, Server} from "@ns";
 import { GetAllServerNames } from "../lib/server";
-import { CrackPorts, IsNukable, PutBundle } from "../lib/attack";
 import { CACHE_PORT } from "../lib/const";
+
+export enum VictimState {
+    NONE,
+    SECURE,
+    BREACHED,
+    PREPPED
+}
 
 export class CacheEntry {
     hostname: string;
@@ -9,13 +15,88 @@ export class CacheEntry {
     backdoorInstalled: boolean;
     purchasedByPlayer: boolean;
     serverGrowth: number;
+    numOpenPortsRequired?: number;
+    openPortCount?: number;
+    isAgent: boolean = false;
+    isVictim: boolean = false;
 
-    constructor(public server: Server) {
+    constructor(public ns: NS, public server: Server) {
         this.hostname = server.hostname;
         this.cpuCores = server.cpuCores;
         this.backdoorInstalled = server.backdoorInstalled ?? false;
         this.purchasedByPlayer = server.purchasedByPlayer;
         this.serverGrowth = server.serverGrowth ?? 0;
+        this.numOpenPortsRequired = server.numOpenPortsRequired ?? 0;
+        this.openPortCount = server.openPortCount ?? 0;
+        this.Rootkit(ns);
+        this.SetRole(ns);
+    }
+
+    SetRole(ns: NS): void {
+        if (!ns.hasRootAccess(this.hostname)) return;
+        if (this.purchasedByPlayer) {
+            this.isAgent = true;
+            this.isVictim = false;
+            return;
+        }
+        if (ns.getServerMaxRam(this.hostname) > 0) this.isAgent = true;
+        if (ns.getServerMaxMoney(this.hostname) > 0) this.isVictim = true;
+    }
+
+    GetTotalRAM(ns: NS, script: string, threads: number = 1): number {
+        threads = Math.round(threads);
+        if (threads < 1 && threads >= Number.POSITIVE_INFINITY) return 0;
+        return threads * ns.getScriptRam(script, this.hostname);
+    }
+
+    CrackPorts(ns: NS): void {
+        if (ns.fileExists("BruteSSH.exe", "home")) ns.brutessh(this.hostname);
+        if (ns.fileExists("FTPCrack.exe", "home")) ns.ftpcrack(this.hostname);
+        if (ns.fileExists("HTTPWorm.exe", "home")) ns.httpworm(this.hostname);
+        if (ns.fileExists("RelaySMTP.exe", "home")) ns.relaysmtp(this.hostname);
+        if (ns.fileExists("SQLInject.exe", "home")) ns.sqlinject(this.hostname);
+    }
+
+    GetSecDiff(ns: NS): number {
+        return ns.getServerSecurityLevel(this.hostname) - ns.getServerMinSecurityLevel(this.hostname);
+    }
+
+    GetMoneyDiff(ns: NS): number {
+        return ns.getServerMaxMoney(this.hostname) - ns.getServerMoneyAvailable(this.hostname);
+    }
+
+    GetOpenRAM(ns: NS): number {
+        if (this.isAgent === false) return 0;
+        return ns.getServerMaxRam(this.hostname) - ns.getServerUsedRam(this.hostname);
+    }
+
+    GetState(ns: NS): VictimState {
+        if (this.isVictim === false) return VictimState.NONE;
+        if (this.GetSecDiff(ns) > 0) return VictimState.SECURE;
+        if (this.GetMoneyDiff(ns) > 0) return VictimState.BREACHED;
+        return VictimState.PREPPED;
+    }
+
+    Rootkit(ns: NS): boolean {
+        if (ns.hasRootAccess(this.hostname)) return true;
+        const portsRequired = this.numOpenPortsRequired ?? 0;
+        if (portsRequired < 0 || portsRequired > 5) return false;
+        this.CrackPorts(ns);
+        if (this.IsNukable()) ns.nuke(this.hostname);
+        if (!ns.hasRootAccess(this.hostname)) return false;
+        this.PutBundle(ns);
+        return true;
+    }
+
+    PutBundle(ns: NS): boolean {
+        if (!this.isAgent) return false;
+        if (ns.scp(ns.ls("home", "/scripts/task/atk_"), this.hostname, "home")) return true;
+        return false;
+    }
+
+    IsNukable(): boolean {
+        if ((this.openPortCount ?? 0) < (this.numOpenPortsRequired ?? Number.POSITIVE_INFINITY)) return false;
+        return true;
     }
 }
 
@@ -34,13 +115,7 @@ export class CacheDB {
         this.entries = new Array<CacheEntry>();
         ns.clearPort(CACHE_PORT);
         const servers = GetAllServerNames(ns);
-        for (const server of servers) {
-            if (!ns.hasRootAccess(server)) {
-                CrackPorts(ns, server);
-                if (IsNukable(ns, server)) if (ns.nuke(server)) PutBundle(ns, server);
-            }
-            this.push(new CacheEntry(ns.getServer(server)));
-        }
+        for (const server of servers) this.push(new CacheEntry(ns, ns.getServer(server)));
         if (!ns.tryWritePort(CACHE_PORT, this)) {
             ns.tprintRaw(`[WARN] CacheDB failed to update server list!`);
         }
