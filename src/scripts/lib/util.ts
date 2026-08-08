50ac0b22481781342c3546e064a31e0b1a07c5e9
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-08-08 18:39:32 -0400

    feat: implement GetWeakenMultiplier and CreateWeakenTable functions for thread calculation

--- src/scripts/lib/util.ts
+++ src/scripts/lib/util.ts
@@ -1,4 +1,25 @@
-export function CalcWeakenThreads(diff: number, cores: number): number {
-    if (diff <= 0 || cores <= 0) return 0;
-    return Math.ceil(diff / (0.05 * (1 + (cores - 1) / 16)));
+import { NS } from "@ns";
+import { WEAKEN_TABLE_PORT } from "./ports";
+
+
+export function GetWeakenMultiplier(ns: NS, cores: number = 1): number {
+    var table = ns.peek(WEAKEN_TABLE_PORT) as Array<number> | string;
+    if (typeof(table) === "string") {
+        if (CreateWeakenTable(ns))
+            table = ns.peek(WEAKEN_TABLE_PORT) as Array<number>;
+        else
+            return 0;
+    }
+    return table[cores];
+}
+
+export function CreateWeakenTable(ns: NS): boolean {
+    const WeakenTable = new Array<number>();
+    WeakenTable.push(0);
+    for (var i = 1; i < 200; i++) {
+        WeakenTable.push(0.05 * (1 + (i - 1) / 16));
+    }
+    ns.clearPort(WEAKEN_TABLE_PORT);
+    if (ns.tryWritePort(WEAKEN_TABLE_PORT, WeakenTable)) return true;
+    return false;
 }
