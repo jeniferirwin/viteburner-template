987f54474c52c00562c3e7ee49dd0e805d1fe5e9
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-08-12 21:30:10 -0400

    refactor: streamline MPSLine processing and enhance output formatting in mpslist

--- src/scripts/cmd/mpslist.ts
+++ src/scripts/cmd/mpslist.ts
@@ -1,15 +1,29 @@
 import {NS} from "@ns";
-import { CacheDB } from "../daemon/cacher";
+import { GetCache } from "../daemon/cacher";
+
+export type MPSLine = {
+    victim: string,
+    seconds: number,
+    money: number,
+    perSec: number
+}
 
 export function main(ns: NS) {
-    const servers = 
+    const servers = GetCache(ns);
+    if (servers === undefined) return;
     const percent = ns.args[0] as number ?? 0.01;
-    ns.tprintRaw(ns.sprintf("%20s %15s %15s %15s", "SERVER", "SECONDS", "MONEY", "BASEMONEY"))
+    const lines = new Array<MPSLine>();
+    ns.tprintRaw(ns.sprintf("%20s %15s %15s %15s", "SERVER", "SECONDS", "MONEY", "MPS"))
     for (const server of servers) {
-        if (!IsVictim(ns, server)) continue;
-        const seconds = ns.getWeakenTime(server) / 1000;
-        const moneyBase = (ns.getServerMaxMoney(server) * percent)
-        const moneyMod = moneyBase * ns.getBitNodeMultipliers().ScriptHackMoney;
-        ns.tprintRaw(ns.sprintf("%20s %15.2fs %15s %15s", server, seconds, ns.format.number(moneyMod), ns.format.number(moneyBase)));
+        if (!server.isVictim) continue;
+        const hostname = server.hostname;
+        const seconds = ns.getWeakenTime(server.hostname) / 1000;
+        const money = ns.getServerMaxMoney(server.hostname) * ns.getBitNodeMultipliers().ScriptHackMoney * percent;
+        const perSec = money / seconds;
+        lines.push({victim: server.hostname, seconds: seconds, money: money, perSec: perSec});
+    }
+    lines.sort((a, b) => b.perSec - a.perSec);
+    for (var line of lines) {
+        ns.tprintRaw(ns.sprintf("%20s %15.2fs %15s %15s", line.victim, line.seconds, ns.format.number(line.money), ns.format.number(line.perSec)));
     }
 }
