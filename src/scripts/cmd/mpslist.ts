275e661b66271b5e6aea94f8b97792d614181343
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-08-13 07:08:03 -0400

    refactor: rename MPSLine to MPSData and update related type definitions in mpslist

--- src/scripts/cmd/mpslist.ts
+++ src/scripts/cmd/mpslist.ts
@@ -1,22 +1,27 @@
 import {NS} from "@ns";
 import { GetCache } from "../daemon/cacher";
+import { CacheEntry, GetOpenRAM } from "../lib/cache";
 
-export type MPSLine = {
+export type MPSData = {
     victim: string,
     seconds: number,
     money: number,
     perSec: number
 }
 
+export function CalculateBatch(ns: NS, cache: CacheEntry[], victim: CacheEntry) {
+    const agents = cache.filter((x) => x.isAgent && GetOpenRAM(ns, x, true) >= 1.7);
+    agents.sort((a, b) => b.cpuCores - a.cpuCores);
+}
+
 export function main(ns: NS) {
     const servers = GetCache(ns);
     if (servers === undefined) return;
     const percent = ns.args[0] as number ?? 0.01;
-    const lines = new Array<MPSLine>();
+    const lines = new Array<MPSData>();
     ns.tprintRaw(ns.sprintf("%20s %15s %15s %15s", "SERVER", "SECONDS", "MONEY", "MPS"))
     for (const server of servers) {
         if (!server.isVictim) continue;
-        const hostname = server.hostname;
         const seconds = ns.getWeakenTime(server.hostname) / 1000;
         const money = ns.getServerMaxMoney(server.hostname) * ns.getBitNodeMultipliers().ScriptHackMoney * percent;
         const perSec = money / seconds;
