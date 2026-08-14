a35cc0f3f0031ccf4d61f503f6754d0d49583a63
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-08-14 13:39:11 -0400

    change ports.ts to config.ts

---
+++ src/scripts/config.ts
@@ -0,0 +1,8 @@
+export const CACHE_PORT = 1;
+export const TARGET_PORT = 2;
+
+export const SCRIPTS = {
+    hack: "scripts/task/atk_hack.js",
+    weaken: "scripts/task/atk_weaken.js",
+    grow: "scripts/task/atk_grow.js"
+}
--- src/scripts/lib/ports.ts
+++
@@ -1,2 +0,0 @@
-export const CACHE_PORT = 1;
-export const TARGET_PORT = 2;
