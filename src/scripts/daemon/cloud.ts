439c4fd3fb1f4531560b26e26178b8c5ee8fe3e8
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-08-18 00:12:10 -0400

    refactor cloud.ts: adjust server purchase cost to improve resource allocation

--- src/scripts/daemon/cloud.ts
+++ src/scripts/daemon/cloud.ts
@@ -5,8 +5,8 @@ export async function main(ns: NS) {
     while (true) {
         var tiers = getCloudTiers();
         if (servers.length < ns.cloud.getServerLimit()) {
-            if (ns.cloud.getServerCost(2 ** 10) < ns.getPlayer().money) {
-                ns.cloud.purchaseServer("entropy", 2 ** 10);
+            if (ns.cloud.getServerCost(2 ** 6) < ns.getPlayer().money) {
+                ns.cloud.purchaseServer("entropy", 2 ** 6);
             }
         }
         for (var server of servers) {
