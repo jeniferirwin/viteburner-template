93ca8356270f250fac6195694e65853437f3af03
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-09-05 13:46:10 -0400

    feat: add initial check for blocked RAM and run haxor script if available

--- src/scripts/dnet/free.ts
+++ src/scripts/dnet/free.ts
@@ -1,6 +1,12 @@
 import { NS } from "@ns";
 
 export async function main(ns: NS) {
+    if (ns.dnet.getBlockedRam() === 0) {
+        if (!ns.scriptRunning("scripts/dnet/haxor.js")) {
+            ns.run("scripts/dnet/haxor.js");
+        }
+        return;
+    }
     while (ns.dnet.getBlockedRam() > 0) {
         await ns.dnet.memoryReallocation();
         if (!ns.scriptRunning("scripts/dnet/haxor.js")) {
