dc0d55546dd5eeb748cba7364a26d9269d6ab481
Jenifer Irwin <jenifer.irwin@gmail.com>
2026-09-05 00:11:47 -0400

    feat: remove obsolete dnet scripts and models to streamline codebase

--- src/scripts/dnet/cache.ts
+++
@@ -1,7 +0,0 @@
-import {NS} from "@ns";
-
-export function main(ns: NS) {
-    for (const file of ns.ls(ns.getHostname(), ".cache")) {
-        ns.dnet.openCache(file);
-    }
-}
--- src/scripts/dnet/models/zerologon.ts
+++
@@ -1,9 +0,0 @@
-import {NS} from "@ns";
-import { RegisterDnetServer, RemoveAuthLock, SudoAuthenticate } from "../dnet";
-
-export async function main(ns: NS): Promise<void> {
-    const server = ns.args[0] as string;
-    const auth = await SudoAuthenticate(ns, server, "");
-    if (auth.success) RegisterDnetServer(ns, server, "");
-    RemoveAuthLock(ns, server);
-}
